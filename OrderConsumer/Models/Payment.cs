448a9cff25433d0988a67c8c5514dd2a5189419e
Bas Dijkstra <bas@ontestautomation.com>
2023-11-30 15:40:25 +0100

    Add BDCT example

---
+++ OrderConsumer/Models/Payment.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderConsumer.Models
+{
+    public class Payment
+    {
+        public Guid Id { get; set; }
+        public Guid OrderId { get; set; }
+        public string Status {  get; set; }
+        public int Amount { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ OrderConsumer/PaymentClient.cs
@@ -0,0 +1,34 @@
+﻿using OrderConsumer.Models;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace OrderConsumer
+{
+    public class PaymentClient
+    {
+        private readonly Uri baseUri;
+
+        public PaymentClient(Uri baseUri)
+        {
+            this.baseUri = baseUri;
+        }
+
+        public async Task<HttpResponseMessage> GetPayment(string id)
+        {
+            using (var client = new HttpClient { BaseAddress = baseUri })
+            {
+                try
+                {
+                    var response = await client.GetAsync($"/payment/{id}");
+                    return response;
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("There was a problem connecting to the PaymentProvider API.", ex);
+                }
+            }
+        }
+    }
+}
